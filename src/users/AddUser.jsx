d674c3c0ecea3aff53cbf0899cb3f65e794edf10
Hassan Amini <108970826+hassan95eb@users.noreply.github.com>
2025-02-01 14:06:16 +0330

    add edit and grt mthod  in axios on addUser comp

--- src/users/AddUser.jsx
+++ src/users/AddUser.jsx
@@ -1,30 +1,94 @@
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import "./addUser.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function AddUser() {
   const params = useParams();
-
+  const navigate = useNavigate();
+  const [data, setData] = useState({
+    name: "",
+    username: "",
+    email: "",
+    address: {
+      city: "",
+      street: "",
+      suite: "",
+      zipcode: "",
+    },
+  });
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!params.id) {
+      axios
+        .post("https://jsonplaceholder.typicode.com/users", data)
+        .then((res) => {
+          console.log(res);
+        });
+    } else {
+      axios
+        .put(`https://jsonplaceholder.typicode.com/users/${params.id}`, data)
+        .then((res) => {
+          console.log(res);
+        });
+    }
+  }
+  useEffect(() => {
+    axios
+      .get(`https://jsonplaceholder.typicode.com/users/${params.id}`)
+      .then((res) => {
+        setData({
+          name: res.data.name,
+          username: res.data.username,
+          email: res.data.email,
+          address: {
+            city: res.data.address.city,
+            street: res.data.address.street,
+            suite: res.data.address.suite,
+            zipcode: res.data.address.zipcode,
+          },
+        });
+      });
+  }, []);
   return (
     <div className="form-section d-flex justify-content-center align-items-center flex-column">
       <h1 className="h2 my-2">{params.id ? "ویرایش کاربر" : "افزودن کاربر"}</h1>
-      <form className="form bg-white rounded p-3">
+      <form onSubmit={handleSubmit} className="form bg-white rounded p-3">
         <div className="mb-3">
-          <label htmlFor="name" className="htmlForm-label">
+          <label htmlFor="name" className="form-label">
             نام و نام خانوادگی
           </label>
-          <input type="text" className="htmlForm-control" id="name" />
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            value={data.name}
+            onChange={(e) => setData({ ...data, name: e.target.value })}
+          />
         </div>
         <div className="mb-3">
-          <label htmlFor="username" className="htmlForm-label">
+          <label htmlFor="username" className="form-label">
             نام کاربری
           </label>
-          <input type="text" className="htmlForm-control" id="username" />
+          <input
+            type="text"
+            className="form-control"
+            id="username"
+            value={data.username}
+            onChange={(e) => setData({ ...data, username: e.target.value })}
+          />
         </div>
         <div className="mb-3">
-          <label htmlFor="email" className="htmlForm-label">
+          <label htmlFor="email" className="form-label">
             ایمیل
           </label>
-          <input type="email" className="htmlForm-control" id="email" />
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            value={data.email}
+            onChange={(e) => setData({ ...data, email: e.target.value })}
+          />
         </div>
         <div className="row">
           <div>
@@ -33,38 +97,73 @@ function AddUser() {
           <div className="col-12 col-md-6 my-1">
             <input
               type="text"
-              className="htmlForm-control"
+              className="form-control"
               id="city"
               placeholder="شهر"
+              value={data.address.city}
+              onChange={(e) =>
+                setData({
+                  ...data,
+                  address: { ...data.address, city: e.target.value },
+                })
+              }
             />
           </div>
           <div className="col-12 col-md-6 my-1">
             <input
               type="text"
-              className="htmlForm-control"
+              className="form-control"
               id="street"
               placeholder="خیابان"
+              value={data.address.street}
+              onChange={(e) =>
+                setData({
+                  ...data,
+                  address: { ...data.address, street: e.target.value },
+                })
+              }
             />
           </div>
           <div className="col-12 col-md-6 my-1">
             <input
               type="text"
-              className="htmlForm-control"
-              id="continue"
+              className="form-control"
+              id="suite"
               placeholder="ادامه آدرس"
+              value={data.address.suite}
+              onChange={(e) =>
+                setData({
+                  ...data,
+                  address: { ...data.address, suite: e.target.value },
+                })
+              }
             />
           </div>
           <div className="col-12 col-md-6 my-1">
             <input
               type="text"
-              className="htmlForm-control"
+              className="form-control"
               id="zipcode"
               placeholder="کدپستی"
+              value={data.address.zipcode}
+              onChange={(e) =>
+                setData({
+                  ...data,
+                  address: { ...data.address, zipcode: e.target.value },
+                })
+              }
             />
           </div>
         </div>
         <div className="d-flex justify-content-end">
-          <button type="submit" className="btn btn-danger px-2 ">
+          <button
+            type="submit"
+            className="btn btn-danger px-2 "
+            onClick={(e) => {
+              e.preventDefault();
+              navigate(-1);
+            }}
+          >
             بازگشت
           </button>
           <button type="submit" className="btn btn-success px-2 me-1">
