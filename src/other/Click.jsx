5b331b7dc18c0db2d28d045dc79fa4ae27555858
Hassan Amini <108970826+hassan95eb@users.noreply.github.com>
2025-02-04 13:53:42 +0330

    use useReducer in simple counter

--- src/other/Click.jsx
+++ src/other/Click.jsx
@@ -1,9 +1,33 @@
-/* eslint-disable react/prop-types */
-const Click = ({ count, handleIncreseCount }) => {
+import { useReducer } from "react";
+let initialState = 0;
+function counterReducer(state, action) {
+  switch (action) {
+    case "incresment":
+      return state + 1;
+    case "decresement":
+      return state - 1;
+    default:
+      return state;
+  }
+}
+const Click = () => {
+  const [count, dispatch] = useReducer(counterReducer, initialState);
   return (
-    <button className="btn btn-info d-block" onClick={handleIncreseCount}>
-      Click : {count}
-    </button>
+    <div>
+      <h1 className="text-center">{count}</h1>
+      <button
+        className="btn btn-success"
+        onClick={() => dispatch("incresment")}
+      >
+        increse
+      </button>
+      <button
+        className="btn btn-danger"
+        onClick={() => dispatch("decresement")}
+      >
+        decrese
+      </button>
+    </div>
   );
 };
 export default Click;
--- src/other/Other.jsx
+++ src/other/Other.jsx
@@ -4,14 +4,14 @@ import Counter from "./Counter";
 import Hover from "./Hover";
 
 const EnhancedHover = Counter(Hover);
-const EnhancedClick = Counter(Click);
 const Others = () => {
   return (
     <div className={`${style.item_content} mt-5 p-4 container-fluid`}>
       <h4 className="text-center"> سایر </h4>
       <hr />
-      <div className="d-flex justify-content-center align-items-center">
-        <EnhancedClick />
+      <div className="d-flex justify-content-center align-items-center flex-column">
+        <Click />
+        <br />
         <EnhancedHover />
       </div>
     </div>
